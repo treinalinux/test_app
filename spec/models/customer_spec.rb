a3eaa6c0c55178f0d45e9f48748953235a5907bb
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-09 11:28:16 -0300

    Working fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  # fixtures :customers
+  fixtures :all
+
+  it 'Create a Customer' do
+    customer = customers(:alan)
+    expect(customer.name).to eq('Alan Alves')
+  end
 end
