3e4e7c6d097b0067a8728fe10ed453313951c5e2
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-14 07:13:09 -0300

    validates description price category if presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :description, :price, :category, presence: true
+
+  def full_description
+    "#{description} - #{price}"
+    # "{product1 - 1.20 }"
+  end
 end
