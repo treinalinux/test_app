5aef9afc6776c33e03d6ccb24e4cb247ff5716a5
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-15 07:57:55 -0300

    Flash Notice

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -26,23 +26,44 @@ RSpec.describe CustomersController, type: :controller do
   end
 
   describe 'as Logged' do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'Flash Notice' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, params: { customer: customer_params }
+      expect(flash[:notice]).to match(/successfully created/)
+    end
+
+    context '#create' do
+      it 'with valid attributes' do
+        customer_params = attributes_for(:customer)
+        sign_in @member
+        expect do
+          post :create, params: { customer: customer_params }
+        end.to change(Customer, :count).by(1)
+      end
+    end
     context '#show' do
       it 'responds a 200 response' do
-        member = create(:member)
-        customer = create(:customer)
+        # member = create(:member)
+        # customer = create(:customer)
 
-        sign_in member
+        sign_in @member
 
-        get :show, params: { id: customer.id }
+        get :show, params: { id: @customer.id }
         expect(response).to have_http_status(200)
       end
       it 'render a :show template' do
-        member = create(:member)
-        customer = create(:customer)
+        # member = create(:member)
+        # customer = create(:customer)
 
-        sign_in member
+        sign_in @member
 
-        get :show, params: { id: customer.id }
+        get :show, params: { id: @customer.id }
         expect(response).to render_template(:show)
       end
     end
