64ede4a554bfac6daa39bc699944d2714a1a8168
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-15 15:47:41 -0300

    Example of config cod Ajax

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -10,9 +10,19 @@ RSpec.feature 'Customers', type: :feature, js: true do
     expect(page).to have_current_path(customers_path)
   end
 
+  # This line added in rails-help for call Ajax
+  # Capybara.default_max_wait_time = 5
+  #
+  #  Example of cod with time waiting 3 seconds in Ajax
+  #  it 'Ajax' do
+  #    visit(customers_path)
+  #    click_link('Add Message')
+  #    expect(page).to have_content('Yes!')
+  #  end
+
   it 'Create a Customer' do
     member = create(:member)
-    login_as(member, :scope => :member)
+    login_as(member, scope: :member)
 
     visit(new_customer_path)
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,11 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.javascript_driver = :chrome
+
+# used for change time default of wait the capybara, the default is 2 seconds,
+# normally used in call on Ajax.
+# Capybara.default_max_wait_time = 5
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
